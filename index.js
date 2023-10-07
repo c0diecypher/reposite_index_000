08b5fa5591509ad66708c923c5e8a677830bba48
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-07 14:10:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -50,14 +50,14 @@ app.get('/web-data', (req, res) => {
   });
 
 app.post('/web-data', async(req, res) => {
-    const {queryId, price} = req.body;
+    const {queryId, price, size, name} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${price}`
+                message_text: ` Поздравляю с покупкой! Вы приобрели ${name} на сумму ${price}₽, размер ${size}US.`
             }
         })
         return res.status(200).json({});
