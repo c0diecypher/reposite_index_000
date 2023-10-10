b116d4ea9412359a5c42bc74b2cc299c6eebeb31
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-10 22:55:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
-const token = '6597024566:AAGXwbkrq2CTnpxPSQIqTlqZQEK9Ur3SBR4';
+const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
 app.use(express.json());
