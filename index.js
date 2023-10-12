3403e689929e5806252a9faa5ce72e92262fb276
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-12 12:16:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -135,21 +135,37 @@ app.get('/getProfilePhoto', (req, res) => {
 });
 
 app.post('/validate-init-data', async (req, res) => {
-    try {
-      // Получить значение заголовка Authorization
-      const authHeader = req.header('Authorization');
-  
-      if (!authHeader || !authHeader.startsWith('twa-init-data')) {
-        return res.status(401).send('Invalid Authorization header');
-      }
-  
-      // Извлечь данные инициализации из заголовка
-      const initData = authHeader.replace('twa-init-data', '').trim();
-  
-      // Добавьте код валидации с использованием CryptoJS
-      const isValid = validate(token, initData);
+  try {
+    // Получить значение заголовка Authorization
+    const authHeader = req.header('Authorization');
 
-    if (isValid) {
+    if (!authHeader || !authHeader.startsWith('twa-init-data')) {
+      return res.status(401).send('Invalid Authorization header');
+    }
+
+    // Извлечь данные инициализации из заголовка
+    const initData = authHeader.replace('twa-init-data', '').trim();
+
+    // Добавьте код валидации с использованием CryptoJS
+    const initDataString = initData;
+    const params = new URLSearchParams(initDataString);
+    const auth_date = params.get('auth_date');
+    const query_id = params.get('query_id');
+    const user = params.get('user');
+    const hash = params.get('hash');
+
+    // Создайте "data-check-string"
+    const data_check_string = `auth_date=${auth_date}\nquery_id=${query_id}\nuser=${user}`;
+
+    // Вычислите секретный ключ
+    const secret_key = CryptoJS.HmacSHA256(token, 'WebAppData').toString();
+
+    // Вычислите подпись HMAC-SHA-256
+    const calculated_hash = CryptoJS.HmacSHA256(data_check_string, secret_key).toString();
+
+    // Сравните calculated_hash с полученным параметром "hash"
+    if (calculated_hash === hash && auth_date >= Math.floor(Date.now() / 1000)) {
+      // Данные получены из Telegram и не устарели
       return res.json({ message: 'Valid initData' });
     } else {
       return res.status(401).json({ error: 'Invalid initData' });
