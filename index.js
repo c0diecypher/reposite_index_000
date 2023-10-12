9a8074967270b67a7b67f2cf128cb1ebc5932088
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-12 19:31:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.post('/validate-initdata', (req, res) => {
     }
 
     // Извлекаем и декодируем данные инициализации из заголовка
-    const initDataString = authorizationHeader.substring('twa-init-data '.length);
-    const initData = JSON.parse(initDataString);
+    const initDataObject = authorizationHeader.substring('twa-init-data '.length);
+    const initData = JSON.parse(initDataObject);
 
     // Выполняем валидацию данных инициализации, используя вашу логику
     const validationResult = validate(initData, token); // Предполагается, что у вас есть функция validate
