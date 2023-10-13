1e789e78cb033042d6a19d826fe4adfb46716d9f
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-13 19:32:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const User = require('./models'); // Импортируйте модель пользователя
 const { validate } = require('@twa.js/init-data-node');
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
@@ -30,8 +31,22 @@ app.post('/validate-initdata', (req, res) => {
 
   try {
     // Выполняем валидацию данных initData
-    validate(initData, token);
-    // Если валидация успешна, вы можете выполнить необходимые действия
+    const validation = validate(initData, token);
+
+    if (validation) {
+      // Парсинг данных initData для получения ID пользователя
+      const userData = JSON.parse(validation.user);
+      const userId = userData.id;
+
+      // Проверяем, существует ли пользователь с данным ID
+      const existingUser = await User.findOne({ where: { id: userId } });
+
+      if (!existingUser) {
+        // Если пользователь не существует, добавляем его в базу данных
+        await User.create({
+          id: userId,
+        });
+      }
 
     res.json({ success: true, message: 'Authorized valid' });
   } catch (error) {
