3383eba95b0c7b006054bc3c030cee147d0c8305
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-13 14:21:02 +0700

    Update models.js

--- models.js
+++ models.js
@@ -2,44 +2,8 @@ const sequelize = require('./database');
 const {DataTypes} = require('sequelize')
 const User = sequelize.define('user',{
 
-    id: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        allowNull: false,
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    first_name: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    last_name: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
+    1
     });
 
-    User.checkAndCreateUser = async (userData) => {
-    const { id } = userData;
-
-    try {
-        const existingUser = await User.findOne({
-            where: { id },
-        });
-
-        if (existingUser) {
-            // Пользователь с данным id уже существует, не создаем новую запись
-            return existingUser;
-        } else {
-            // Создаем новую запись
-            const newUser = await User.create(userData);
-            return newUser;
-        }
-    } catch (error) {
-        throw error;
-    }
-};
 
 module.exports = User; 
