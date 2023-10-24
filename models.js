e3125e08914e85e2a1da585e305eee03bca53484
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-24 11:13:57 +0700

    Update models.js

--- models.js
+++ models.js
@@ -1,12 +1,26 @@
 const sequelize = require('./database');
 const { DataTypes } = require('sequelize');
 
-const User = sequelize.define('user', {
-  id: {
-    type: DataTypes.INTEGER,
+const User = sequelize.define('User', {
+  userId: {
+    type: DataTypes.STRING,
     allowNull: false,
-    primaryKey: true,
+    unique: true,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: true, // Разрешить значение быть null, если данные не доступны
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: true, // Разрешить значение быть null, если данные не доступны
   },
 });
+
+User.sync({ force: false });
 
 module.exports = User;
