2acbafa3cc5bee907a34e701fe362f46c1251905
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-07 21:42:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ app.post('/customer/settings/client/buy/offer/pay', async (req, res) => {
             const { id, link } = paymentResponse.data;
 
             // Создаем URL для второго запроса
-            const secondUrl = `https://p2pkassa.online/payment/${id}`;
+            const secondUrl = `https://p2pkassa.online/payment/${ProductOrder}`;
 
             // Отправляем второй POST-запрос
             const secondResponse = await axios.post(secondUrl);
