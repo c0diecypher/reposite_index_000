9f3933777fa11bedbccc2eb7a962777f83af064d
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-09 19:25:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ app.post('/customer/settings/client/buy/offer/pay', async (req, res) => {
 
               const sign = crypto
                                     .createHash('sha256')
-                                    .update(`${getPaymentId}:${getPaymentOrderId}:${projectId}:${apiKey}`)
+                                    .update(`${getPaymentId}:${getPaymentOrderId}:${project_id}:${apikey}`)
                                     .digest('hex');
               console.log(sign)
               // Проверка подписи
