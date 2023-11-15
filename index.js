70882d5e2331cf1e2dd6b0753c9b00ffc9e8227c
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-15 17:11:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -246,8 +246,12 @@ Zipper App снова ждет ваших заказов! ⚡`;
               const getPayment = await axios.post('https://p2pkassa.online/api/v1/getPayment', dataToPayment, config);
               resGetPayment = getPayment.data;
               console.log(resGetPayment);
-              const status = resGetPayment.status;
-              console.log('Статус оплаты:', status);
+              if (resGetPayment && resGetPayment.status !== undefined) {
+                const status = resGetPayment.status;
+                console.log('Статус оплаты:', status);
+              } else {
+                console.log('Статус оплаты не найден или равен undefined.');
+              }
               // Создаем URL для второго запроса
               const getPaymentId = resGetPayment.id;
               const getPaymentOrderId = resGetPayment.order_id;
