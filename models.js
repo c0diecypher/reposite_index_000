d073b7e34980ca705625affe87558a99e86c8b66
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-17 16:54:22 +0700

    Update models.js

--- models.js
+++ models.js
@@ -48,7 +48,7 @@ const User = sequelize.define('User', {
     unique: true,
   },
   userOrder: {
-    type: DataTypes.TEXT,
+    type: DataTypes.STRING(500),
     allowNull: true,
   },
   userOrderStatus: {
