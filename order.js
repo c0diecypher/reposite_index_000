48db2594ca6e22b7ec42413298ae257e5cdff42c
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-20 01:29:36 +0700

    Update order.js

--- order.js
+++ order.js
@@ -2,35 +2,9 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const User = require('./models');
-const { EventEmitter } = require('events');
-
-const eventEmitter = new EventEmitter();
-
+const User = require('./models'); 
 router.use(express.json());
 router.use(cors());
-
-// SSE endpoint
-
-router.get('/sse', (req, res) => {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-    const listener = (data) => {
-        console.log('Получено обновление от сервера:', data);
-        res.write(`data: ${JSON.stringify(data)}\n\n`);
-    };
-    eventEmitter.addListener('paymentUpdate', listener);
-    
-    res.on('finish', () => {
-        eventEmitter.removeListener('paymentUpdate', listener);
-    });
-});
-
-// Обработка обновлений статуса и отправка через SSE
-const sendPaymentUpdate = (status) => {
-  eventEmitter.emit('paymentUpdate', { status });
-};
 
 router.post('/get/payment', async (req, res) => {
     const { userId, order_id } = req.body;
@@ -40,14 +14,11 @@ router.post('/get/payment', async (req, res) => {
 
         if (user) {
             const userOrderArray = JSON.parse(user.userOrder);
-
+        
             const order = userOrderArray.find(order => order.order_id === order_id);
-
+        
             if (order) {
                 res.json({ status: order.status });
-                console.log('Отправлено обновление:', order.status);
-                // Отправка обновления через SSE
-                sendPaymentUpdate(order.status);
             } else {
                 res.status(404).json({ error: 'Заказ не найден' });
             }
@@ -60,6 +31,7 @@ router.post('/get/payment', async (req, res) => {
     }
 });
 
+
 router.post('/update/payment', async (req, res) => {
     const { userId, order_id } = req.body;
 
@@ -68,15 +40,13 @@ router.post('/update/payment', async (req, res) => {
 
         if (user) {
             const userOrderArray = JSON.parse(user.userOrder);
-
+        
             const order = userOrderArray.find(order => order.order_id === order_id);
-
+        
             if (order) {
+                // Обновление статуса или других данных платежа
+                // Например, order.status = 'PAID';
                 res.json({ status: order.status });
-
-                // Отправка обновления через SSE
-                sendPaymentUpdate(order.status);
-                console.log('Отправлено обновление:', order.status);
             } else {
                 res.status(404).json({ error: 'Заказ не найден' });
             }
@@ -84,7 +54,7 @@ router.post('/update/payment', async (req, res) => {
             res.status(404).json({ error: 'Пользователь не найден' });
         }
     } catch (error) {
-        console.error('Ошибка при запросе статуса из базы данных:', error);
+        console.error('Ошибка при обновлении данных платежа:', error);
         res.status(500).json({ error: 'Внутренняя ошибка сервера' });
     }
 });
