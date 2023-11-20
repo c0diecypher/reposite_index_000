d67270ffcebabd8579fcf6972c39da6a6cd8d6ad
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-20 13:31:01 +0700

    Update order.js

--- order.js
+++ order.js
@@ -59,4 +59,33 @@ router.post('/update/payment', async (req, res) => {
     }
 });
 
+router.post('/load/basket', async (req, res) => {
+    const { userId } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { userId: userId.toString() } });
+
+        if (user) {
+            const userOrderArray = JSON.parse(user.userOrder);
+
+            // Получить только определенные поля для товаров с статусом "WAIT"
+            const waitOrders = await Order.findAll({
+                attributes: ['id', 'name', 'order_id', 'price', 'size', 'status'],
+                where: {
+                    order_id: userOrderArray.map(order => order.order_id),
+                    status: 'WAIT',
+                }
+            });
+
+            // Отправить данные на клиент
+            res.status(200).json(waitOrders);
+        } else {
+            res.status(404).json({ error: 'Пользователь не найден' });
+        }
+    } catch (error) {
+        console.error('Ошибка при обновлении данных платежа:', error);
+        res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+    }
+});
+
 module.exports = router;
