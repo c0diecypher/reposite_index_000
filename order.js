f8fe3d514b78dfad50bc78b842475facf5613ced
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-20 13:55:57 +0700

    Update order.js

--- order.js
+++ order.js
@@ -69,10 +69,9 @@ router.post('/load/basket', async (req, res) => {
             const userOrderArray = JSON.parse(user.userOrder);
 
             // Получить только определенные поля для товаров с статусом "WAIT"
-            const waitOrders = await userOrder.findAll({
+            const waitOrders = await userOrderArray.findAll({
                 attributes: ['id', 'name', 'order_id', 'price', 'size', 'status'],
                 where: {
-                    order_id: userOrderArray.map(order => order.order_id),
                     status: 'WAIT',
                 }
             });
