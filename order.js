8beb1f7508d459cae8773719b71487b9e0171c27
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-20 17:23:30 +0700

    Update order.js

--- order.js
+++ order.js
@@ -100,4 +100,111 @@ router.post('/load/basket', async (req, res) => {
     }
 });
 
+router.post('/customer/settings/client/buy/offer/pay/basket', async (req, res) => {
+    const { productId,queryId, price, size, name, userId, order_id, time } = req.body;
+    console.log(productId,queryId, price, size, name, userId, order_id);
+  
+    // Проверьте, что userId совпадает с ожидаемым
+    const allowedUserId = userId;
+    if (userId !== allowedUserId) {
+        return res.status(403).json({ error: 'Доступ запрещен', message: 'Вы не имеете разрешения на выполнение этой операции.' });
+    }
+
+    try {
+        const apikey = 'cpfmxaq0su2dy63v4g9zowjh';
+        const project_id = '225';
+        console.log(project_id, apikey);
+        const ProductName = name;
+        const ProductSize = size;
+        ProductOrder = order_id;
+        const ProductPrice = price.replace(/\s/g, '').replace(/\u00a0/g, '');
+        console.log(ProductPrice);
+        console.log(ProductOrder);
+        console.log(ProductSize);
+        console.log(ProductName);
+        const config = {
+                  headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+                };
+        
+        // Поиск пользователя в базе данных
+        const user = await User.findOne({ where: { userId: userId.toString() } });
+
+        if (user) {
+            // Извлекаем данные пользователя
+            const userId = user.userId;
+            const userFio = user.userFio || 'Не указано';
+            const userAdress = user.userAdress || 'Не указано';
+            const phoneNumber = user.phoneNumber || 'Не указано';
+            const userCity = user.userCity || 'Не указано';
+            const desc = `Название товара: ${ProductName}, 
+                      размер: ${ProductSize}, 
+                      ФИО: ${userFio}, 
+                      Номер для связи ${phoneNumber}
+                      Город: ${userCity},
+                      Адрес доставки: ${userAdress}`;
+            const params = `
+      Поздравляем с покупкой!
+      📋 Данные заказа:
+🧾 ${ProductName}, 
+🎟️ ${ProductOrder}, 
+📏 ${ProductSize}, 
+💎 ${ProductPrice}.
+      🚚 Детали доставки:
+👤 ${userFio},
+📱 ${phoneNumber},
+🏙️ ${userAdress},
+📍 ${userCity}
+ID: ${userId}.
+
+Zipper App снова ждет ваших заказов! ⚡`;
+
+            const dataToSend = {
+                  project_id: project_id,
+                  order_id: ProductOrder, // Используйте order_id из req.body
+                  amount: ProductPrice,
+                  apikey: apikey,
+                  desc: desc,
+                  data: params,
+              };
+          
+            const response = await axios.post('https://p2pkassa.online/api/v1/link', dataToSend, config);
+            const result = response.data;
+             console.log(result);
+            if (result && result.link && result.id) {
+              // Создаем URL для второго запроса
+              const paymentUrl = result.link;
+              paymentId = result.id;
+              console.log(paymentUrl);
+              console.log(paymentId);
+              // Отправляем второй POST-запрос
+
+              const dataToPayment = {
+                id: paymentId,
+                project_id: project_id,
+                apikey: apikey
+              };
+              const getPayment = await axios.post('https://p2pkassa.online/api/v1/getPayment', dataToPayment, config);
+              const resGetPayment = getPayment.data;
+
+              
+               return res.json({ paymentUrl });  
+            } else {
+              
+              console.log('Отсутствуют данные id и link в ответе');
+              
+            }
+        } else {
+            // Если пользователь не найден, обработка ошибки или возврат 404
+            return res.status(400).json({ error: 'Ошибка', message: 'Пользователь не найден.' });
+        }
+    } catch (error) {
+        // Обработка ошибки
+        console.error(error);
+        return res.status(500).json({ error: 'Ошибка', message: 'Внутренняя ошибка сервера.' });
+    }
+});
+
+
 module.exports = router;
