00eb574e50dc9f59412e5829d5d1f39d6df41ff7
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-24 22:11:44 +0700

    Update order.js

--- order.js
+++ order.js
@@ -303,5 +303,25 @@ router.post('/load/basket/paid', async (req, res) => {
     }
 });
 
+router.post('/get/bonus', async (req, res) => {
+    const { userId } = req.body;
+
+    try {
+        // Ищем пользователя по userId
+        const user = await User.findOne({ userId });
+
+        if (!user) {
+            return res.status(404).json({ message: 'Пользователь не найден' });
+        }
+
+        // Возвращаем userBonus
+        return res.status(200).json({ userBonus: user.userBonus });
+    } catch (error) {
+        console.error('Ошибка при обработке запроса /get/bonus:', error);
+        res.status(500).json({ message: 'Произошла ошибка' });
+    }
+});
+
+
 
 module.exports = router;
