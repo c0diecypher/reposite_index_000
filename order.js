a66293c581b2d0ccc7cdbeae7b3a65e2a61d3275
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-25 18:02:16 +0700

    Update order.js

--- order.js
+++ order.js
@@ -194,7 +194,7 @@ router.post('/get/bonus', async (req, res) => {
 
         // Выбираем, какой referralId использовать (в данном случае, берем первый элемент)
         const primaryReferralId = referralIds[0];
-        console.log('id, primaryReferralId);
+        console.log('id', primaryReferralId);
         // Если есть referralId, обновляем userId
         if (primaryReferralId) {
             const referredUser = await User.findOne({ where: { userId: primaryReferralId.toString() } });
