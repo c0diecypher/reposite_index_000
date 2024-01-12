c5dd3bdf5e35b9c71b1324c1a6b61f121a1e6433
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2024-01-12 17:10:50 +0700

    Update order.js

--- order.js
+++ order.js
@@ -34,6 +34,31 @@ router.post('/get/payment', async (req, res) => {
     }
 });
 
+router.get('/get/status', async (req, res) => {
+    const { userId, order_id } = req.query;
+
+    try {
+        const user = await User.findOne({ where: { userId: userId.toString() } });
+
+        if (user) {
+            const userOrderArray = JSON.parse(user.userOrder);
+        
+            const order = userOrderArray.find(order => order.order_id === order_id);
+        
+            if (order) {
+                res.json({ status: order.status });
+            } else {
+                res.status(404).json({ error: 'Заказ не найден' });
+            }
+        } else {
+            res.status(404).json({ error: 'Пользователь не найден' });
+        }
+    } catch (error) {
+        console.error('Ошибка при запросе статуса из базы данных:', error);
+        res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+    }
+});
+
 
 router.post('/update/payment', async (req, res) => {
     const { userId, order_id } = req.body;
